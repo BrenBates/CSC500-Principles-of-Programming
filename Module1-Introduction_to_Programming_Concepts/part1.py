8e4bc4473f91637fd870aee9c8dd9231f2f848dc
Bren-lap <brennenbates2859@gmail.com>
2024-09-06 14:59:47 -0500

    Completed assignment

---
+++ Module1-Introduction_to_Programming_Concepts/part1.py
@@ -0,0 +1,14 @@
+# Brennen Bates
+# 9/6/2024
+
+# Part 1:
+# Write a Python program to find the addition and subtraction of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. Also, subtract the two numbers to find the output.
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+
+print(num1,'+',num2,'is',num1+num2)
+print(num1,'-',num2,'is',num1-num2)

3f6745cabfccf171d8d1682ecaab2d6862a5b34a
Vacant <brennenbates2859@gmail.com>
2024-09-06 15:09:02 -0500

    Add files via upload

---
+++ CSC500-Principles-of-Programming/Module1-Introduction_to_Programming_Concepts/part2.py
@@ -0,0 +1,16 @@
+# Brennen Bates
+# 9/6/2024
+
+# Part 2:
+# Write a Python program to find the multiplication and division of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, multiply them together to find the output. Also, divide num1/num2 to find the output.
+
+# Compile and submit your pseudocode, source code, and screenshots of the application executing the code from parts 1 and 2, the results and GIT repository in a single document (Word is preferred).
+
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter another number:"))
+
+
+print(num1,'*',num2,'is',num1*num2)
+print(num1,'/',num2,'is',num1/num2)
